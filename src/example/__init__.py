aae74effc3fab008bd917874e3209bbfa8b96c22
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-09-03 14:02:05 +0100

    Initial commit

---
+++ src/example/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about example."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Rich FitzJohn"
+__email__ = "rich.fitzjohn@gmail.com"
+__version__ = "0.1.0"
